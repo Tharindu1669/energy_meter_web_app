6fe5ddb22edb0a5632d8027b929b90502cf4e8b5
Tharindu1669 <47104001+Tharindu1669@users.noreply.github.com>
2021-11-25 22:23:30 +0530

    energy calculations

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import datetime
 import calendar
 import pandas as pd
 import time
+from __future__ import division
 
 mydb = mysql.connector.connect(
   host="localhost",
@@ -11,20 +12,30 @@ mydb = mysql.connector.connect(
   database="db_kettle_mode"
 )
 
-from fastapi import FastAPI
-
 app = FastAPI()
-
+unixtime = 0;
 
 @app.get("/")
-async def root(phase_1: float = 0):
+async def root(phase_1,phase_2,phase_3,voltage,powerPh1,powerPh2,powerPh3, deltaTime):
 
-	mycursor = mydb.cursor()
+	mycursor1 = mydb.cursor()
+	mycursor2 = mydb.cursor()
 
 	unixtime = datetime.datetime.utcnow()
-	sql = "INSERT INTO realtime_consumption (phase_1, time) VALUES (%s, %s)"
-	print(unixtime)
-	val = (phase_1, unixtime)
-	mycursor.execute(sql, val)
+	sql1 = "INSERT INTO realtime_consumption (phase_1, phase_2, phase_3, voltage, powerPh1, powerPh2, powerPh3, time) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
+	val1 = (phase_1, phase_2, phase_3,voltage,powerPh1,powerPh2,powerPh3, unixtime)
+
+
+
+	energyPh1 = powerPh1 * deltaTime 
+	energyPh2 = powerPh2 * deltaTime
+	energyPh3 = powerPh3 * deltaTime
+	totalEnergy = energyPh1 + energyPh2 + energyPh3
+	sql2 = "INSERT INTO energy_consumption (time, EnergyPhase1, EnergyPhase2, EnergyPhase3, TotalEnergy) VALUES (%s, %s, %s, %s, %s, %s)"
+	val2 = (unixtime, energyPh1, energyPh2, energyPh3, totalEnergy )
+
+	mycursor1.execute(sql1, val1)
+	mycursor2.execute(sql2, val2)
+	previousTime = datetime.datetime.utcnow()
 	mydb.commit()
 	return {"message": "Success"}
